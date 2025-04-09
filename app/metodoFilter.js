0fe1a6fbb7d88932a20ba7cf7354570864a4de81
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-04-09 10:39:27 -0300

    feat: filter book list on button click

---
+++ app/metodoFilter.js
@@ -0,0 +1,15 @@
+const botoes = document.querySelectorAll(".btn");
+botoes.forEach(botao => {
+    botao.addEventListener("click", filtrarLivros)
+})
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value;
+
+    let livrosFiltrados = livros.filter(livro => {
+        return livro.categoria === categoria;
+    })
+    
+    exibirOsLivrosNaTela(livrosFiltrados)
+}
