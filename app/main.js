3b901cc6f19ce0ba4165c97ccb7992041161770e
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-04-09 10:14:45 -0300

    feat: apply discount to all book prices

--- app/main.js
+++ app/main.js
@@ -4,30 +4,10 @@ const endpointDaAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.js
 
 //Faz a requisição e busca os livros da API
 getBuscarLivrosDaAPI()
-
-const elementoParaInserirLivros = document.getElementById("livros");
-
 async function getBuscarLivrosDaAPI(){
     const res = await fetch(endpointDaAPI); //fetch -> manipular as funções e as requisições HTTP
     livros = await res.json();
+    let livrosComDesconto = aplicarDesconto(livros);
     // console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 } 
-
-function exibirOsLivrosNaTela(listaDeLivros){
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">
-                ${livro.titulo}
-            </h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">${livro.preco}</p>
-            <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>
-        ` 
-    })
-}
