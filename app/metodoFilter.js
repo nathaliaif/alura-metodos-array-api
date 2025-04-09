231cf65b0864f0cd76d1c73c1f9965004ed457fe
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-04-09 12:09:56 -0300

    feat: filter all books that are still available

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -7,9 +7,6 @@ function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
 
-    let livrosFiltrados = livros.filter(livro => {
-        return livro.categoria === categoria;
-    })
-    
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
 }
